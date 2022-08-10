4fd26a47439309430c9ac1afb8fb974c00b989d9
mutharibayub <mutharib2002@gmail.com>
2022-08-10 18:40:56 +0500

    Added recursive removal to removeExes

--- util/removeExes.py
+++ util/removeExes.py
@@ -1,5 +1,10 @@
 import os
 
-for file in os.listdir("."):
-    if file.replace('.exe','`')[-1]=='`':
-        os.remove(file)
+def removeExes(folder_path):
+    for file in os.listdir(folder_path):
+        if os.path.isdir(os.path.join(folder_path,file)):
+            removeExes(os.path.join(folder_path,file))
+        elif file.replace('.exe','`')[-1]=='`':
+            os.remove(os.path.join(folder_path,file))
+
+removeExes(".")
