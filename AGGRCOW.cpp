0163fb6c91ca7596e6b0eeb7ff96cecf75a980aa
mutharibayub <mutharib2002@gmail.com>
2022-08-10 20:00:17 +0500

    Updated AGGRCOW with correct solution + cleanup of wrong/ bruteforce solutions

--- AGGRCOW.cpp
+++ AGGRCOW.cpp
@@ -1,117 +1,40 @@
 #include <vector>
 #include <algorithm>
-#include <queue>
 #include <iostream>
 using namespace std;
 
-template <class T>
-class node
+bool checkDist(const vector<int>& locs, int cows, int dist)
 {
-public:
-    T val;
-    bool valid;
-    node *back;
-    node *front;
-    node(T val, bool valid=true, node *back=nullptr, node *front=nullptr):val(val),valid(valid),back(back),front(front){}
-    bool operator<(const node& obj)
+    cows--;
+    int distTillNow = 0;
+    for(int i=1;i<locs.size();i++)
     {
-        return this->val < obj.val;
+        distTillNow += locs[i]-locs[i-1];
+        if(distTillNow >= dist)
+        {
+            cows--;
+            distTillNow = 0;
+        }
     }
-};
-
-template<class T>
-class Compare
-{
-public:
-    bool operator() (const node<T>* obj1, const node<T>* obj2)
-    {
-        return obj1->val > obj2->val;
-    }
-};
+    return cows<=0;
+}
 
 void testCase()
 {
     int n,c;
     cin>>n>>c;
-    node<int> *start=nullptr;
-    priority_queue<node<int>*, vector<node<int>*>, Compare<int>> pq;
     vector<int> locs(n);
     for(int i=0;i<n;i++)
     {
         cin>>locs[i];
     }
     sort(locs.begin(),locs.end());
-    for(int i=n-1;i>0;i--)
+    int minDist=0;
+    for(int jump=locs.back()-locs.front();jump>0;jump/=2)
     {
-        node<int> *tempNode = new node<int>(locs[i]-locs[i-1]);
-        pq.push(tempNode);
-        if(start==nullptr)
-        {
-            start = tempNode;
-        }
-        else
-        {
-            tempNode->front = start;
-            start->back = tempNode;
-            start = tempNode;
-        }
+        while(checkDist(locs, c, minDist+jump))minDist += jump;
     }
-    vector<node<int>*> removedNodes;
-    for(int i=0;i<(n-c);i++)
-    {
-        while(pq.top()->valid==false)
-        {
-            pq.pop();
-        }
-        node<int> *tempNode = pq.top();
-        pq.pop();
-        if(tempNode->back==nullptr || (tempNode->front!=nullptr && tempNode->back->val > tempNode->front->val))
-        {
-            cout<<tempNode->val<<" "<<tempNode->front->val;
-            tempNode->front->valid=false;
-            tempNode->val += tempNode->front->val;
-            cout<<" = "<<tempNode->val<<'\n';
-            removedNodes.push_back(tempNode->front);
-            tempNode->front = tempNode->front->front;
-            if(tempNode->front)tempNode->front->back = tempNode;
-            pq.push(tempNode);
-        }
-        else
-        {
-            cout<<tempNode->val<<" "<<tempNode->back->val;
-            tempNode->back->valid=false;
-            tempNode->val += tempNode->back->val;
-            cout<<" = "<<tempNode->val<<'\n';
-            removedNodes.push_back(tempNode->back);
-            tempNode->back = tempNode->back->back;
-            if(tempNode->back)tempNode->back->front = tempNode;
-            pq.push(tempNode);
-        }
-    }
-    while(pq.top()->valid==false)
-    {
-        pq.pop();
-    }
-    int ans = pq.top()->val;
-    cout<<ans<<'\n';
-    for(auto it:removedNodes)
-    {
-        // cout<<it->val<<'\t';
-        delete it;
-    }
-    // cout<<'\n';
-    start = pq.top();
-    while(start->back!=nullptr)
-    {
-        start = start->back;
-    }
-    while(start)
-    {
-        // cout<<start->val<<'\t';
-        node<int> *temp = start;
-        start = start->front;
-        delete temp;
-    }
+    cout<<minDist<<'\n';
 }
 
 int main()
