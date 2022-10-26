e8729a878dc0a04b5c38865d940d4f5e38af2c84
mutharibayub <mutharib2002@gmail.com>
2022-10-26 11:58:03 +0500

    codeForces: Added round829div2 Solution 5

---
+++ codeForces/round829div2XX/5.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+#define ll long long
+
+const ll MOD = 998244353;
+
+ll getInv(ll num, ll mod)
+{
+    num%=mod;
+    if(num<0)num+=mod;
+    ll Q,R=1,T1=0,T2=1,T,tmp=mod;
+    while(R)
+    {
+        Q=mod/num;
+        R=mod%num;
+        T=T1-T2*Q;
+        mod=num;
+        num=R;
+        T1=T2;
+        T2=T;
+    }
+    return T1>=0?T1%tmp:T1%tmp+tmp;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, zeros=0;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+            zeros+=arr[i]==0;
+        }
+        
+        int j=0;
+        for(int i=0;i<zeros;i++)
+        {
+            j+=arr[i]==1;
+        }
+        if(!j)
+        {
+            cout << 0 << '\n';
+            continue;
+        }
+        
+        ll val = (1ll*n*(n-1))%MOD*getInv(2, MOD)%MOD;
+        for(int i=2;i<=j;i++)
+        {
+            ll tmp = ((1ll*n*(n-1))%MOD*getInv(2ll*i*i, MOD))%MOD;
+            val = (val+tmp)%MOD;
+        }
+        
+        cout << val << '\n';
+
+    }
+
+    return 0;
+}
