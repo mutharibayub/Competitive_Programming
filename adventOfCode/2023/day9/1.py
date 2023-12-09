1d63d7f5b4bc5097e547ceb40c55e7da1ce5df21
mutharibayub <mutharib2002@gmail.com>
2023-12-09 17:03:19 +0500

    adventOfCode: Adds 2023 day9

---
+++ adventOfCode/2023/day9/1.py
@@ -0,0 +1,33 @@
+import os
+
+os.chdir("./adventOfCode/2023/day9")
+
+def getNext(sequence):
+    
+    diff = []
+    same = True
+    for i in range(len(sequence)-1):
+        diff.append(sequence[i+1] - sequence[i])
+        if i > 0:
+            same = (same and diff[-1] == diff[-2])
+    if same:
+        return diff[-1] + sequence[-1]
+    
+    return getNext(diff) + sequence[-1]
+
+def solve(inp):
+    ans = 0
+    
+    sequences = [[int(ele.strip()) for ele in ele1.strip().split(" ") if ele.strip() != ""] for ele1 in inp.split("\n")]
+    for sequence in sequences:
+        ans += getNext(sequence)
+    
+    return ans
+
+with open("sample1.txt", "r") as f:
+    ans = solve(f.read())
+    print(ans)
+    assert(ans == int(open("sampleAnswer1.txt", "r").read().strip()))    
+    
+with open("big.txt", "r") as f:
+    print(solve(f.read()))
